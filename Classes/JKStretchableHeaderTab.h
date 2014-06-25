443bfb5865583f6c47420f4af801e77e4f2c6823
Joel Koroniak <joelkoroniak@mac.com>
2014-06-25 15:37:50 -0400

    - Added an .h file for more convenient imports

---
+++ Classes/JKStretchableHeaderTab.h
@@ -0,0 +1,13 @@
+//
+//  JKStretchableHeaderTab.h
+//  Pods
+//
+//  Created by Joel Koroniak on 2014-06-25.
+//
+//
+
+#import "JKStretchableHeaderTabViewController.h"
+#import "JKStretchableHeaderView.h"
+#import "JKTabBar.h"
+#import "JKTabBarItemButton.h"
+#import "JKTabViewController.h"
